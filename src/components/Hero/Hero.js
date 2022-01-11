79fceab91c617684848325bf086fdc516d79cf2c
Nahidul <nahisl12@gmail.com>
2022-01-11 17:07:19 +0000

    minor style changes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,28 +5,26 @@ import Button from "../Helpers/Button";
 
 const Hero = () => {
   return (
-    <>
-      <header className="hero-container">
-        {/* <NavBar /> */}
-        <div className="description-container">
-          <h5>BRAND, DEV, ECOM, MARKETING</h5>
+    <header className="hero-container">
+      {/* <NavBar /> */}
+      <div className="description-container">
+        <h5>BRAND, DEV, ECOM, MARKETING</h5>
 
-          <h2 id="title-one">We unleash</h2>
-          <h2 id="title-two">business potential</h2>
+        <h2 id="title-one">We unleash</h2>
+        <h2 id="title-two">business potential</h2>
 
-          <p>
-            We create brand experiences which are memorable and distinct. Our experienced team create and
-            develop brands with personality and resonance.
-          </p>
+        <p>
+          We create brand experiences which are memorable and distinct. Our experienced team create and
+          develop brands with personality and resonance.
+        </p>
 
-          <Button text="Let's talk" url="/" />
-        </div>
+        <Button text="Let's talk" url="/" />
+      </div>
 
-        <div className="image-container">
-          <img src={heroImage} className="hero-picture" alt="spaniel-hero" />
-        </div>
-      </header>
-    </>
+      <div className="image-container">
+        <img src={heroImage} className="hero-picture" alt="spaniel-hero" />
+      </div>
+    </header>
   );
 };
 
