fb92e017d7f310cb2c7485c432b8eddf7b4cde18
Nahidul <nahisl12@gmail.com>
2022-01-11 16:10:03 +0000

    Updates

---
+++ precache-manifest.12ab87b5aff691845864eb3850709765.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e07eb596e88dbce2ed1c8ec7f3d59a1",
+    "url": "/digital-spaniel/index.html"
+  },
+  {
+    "revision": "bc4d7510b1b8739de3de",
+    "url": "/digital-spaniel/static/css/main.6de9ae4b.chunk.css"
+  },
+  {
+    "revision": "c55fef89b24b18bd13bb",
+    "url": "/digital-spaniel/static/js/2.d199fe56.chunk.js"
+  },
+  {
+    "revision": "337690bdb71522eaada8e2b2e5b3e79a",
+    "url": "/digital-spaniel/static/js/2.d199fe56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b07b8972e4877521972",
+    "url": "/digital-spaniel/static/js/3.7fc12770.chunk.js"
+  },
+  {
+    "revision": "bc4d7510b1b8739de3de",
+    "url": "/digital-spaniel/static/js/main.e889fbb4.chunk.js"
+  },
+  {
+    "revision": "446ed57056897897cd80",
+    "url": "/digital-spaniel/static/js/runtime-main.0bef734d.js"
+  },
+  {
+    "revision": "1ccc79eb2c2079a3a55678c664dd430a",
+    "url": "/digital-spaniel/static/media/Spaniel01_gradient.1ccc79eb.png"
+  },
+  {
+    "revision": "beed1b66a2a68aea01f9dcf790c904c9",
+    "url": "/digital-spaniel/static/media/dashboard01@2x.beed1b66.png"
+  },
+  {
+    "revision": "9113b6b5985107920d104357fd7a93d2",
+    "url": "/digital-spaniel/static/media/lab01.9113b6b5.png"
+  },
+  {
+    "revision": "42ded10a7630e523dcb87a0e5b696b1b",
+    "url": "/digital-spaniel/static/media/logo.42ded10a.png"
+  },
+  {
+    "revision": "53e93e45d947963cf7c3424960a10fb7",
+    "url": "/digital-spaniel/static/media/makerek.53e93e45.png"
+  },
+  {
+    "revision": "8fb57228a2bb64dbb67ab8fe322cee6e",
+    "url": "/digital-spaniel/static/media/newspaper.8fb57228.png"
+  },
+  {
+    "revision": "e8679e14dc7560189ea0e5cd87690fb0",
+    "url": "/digital-spaniel/static/media/newspaper02.e8679e14.png"
+  },
+  {
+    "revision": "b52dc794d1078853c349a1c4d7475a31",
+    "url": "/digital-spaniel/static/media/office01.b52dc794.png"
+  },
+  {
+    "revision": "55b365d20465e9a7c0d2c999cd34ef71",
+    "url": "/digital-spaniel/static/media/office02.55b365d2.png"
+  },
+  {
+    "revision": "15b6686c7ba29bfbb3f4c5ab52ea36ed",
+    "url": "/digital-spaniel/static/media/office03.15b6686c.png"
+  },
+  {
+    "revision": "f51f437fdfc8fbc8c60c5e3c9c33b0d6",
+    "url": "/digital-spaniel/static/media/office04.f51f437f.png"
+  },
+  {
+    "revision": "09ffd1efb1be1c8a1635680e77c7b806",
+    "url": "/digital-spaniel/static/media/rider01.09ffd1ef.png"
+  },
+  {
+    "revision": "7ebaa4b1b3883db7b960fbd08d27f2f2",
+    "url": "/digital-spaniel/static/media/socks.7ebaa4b1.png"
+  },
+  {
+    "revision": "b0737f77d52faffc0bf1ef4810adb731",
+    "url": "/digital-spaniel/static/media/testimonial_face01.b0737f77.png"
+  },
+  {
+    "revision": "15e8e94052149009214854e5d2099dd4",
+    "url": "/digital-spaniel/static/media/testimonial_face02.15e8e940.png"
+  },
+  {
+    "revision": "f4793682dce467a0f7b0ccf3f4f9b33c",
+    "url": "/digital-spaniel/static/media/testimonial_face03.f4793682.png"
+  },
+  {
+    "revision": "5b0f83fba87f87d78a427dfc1617cb30",
+    "url": "/digital-spaniel/static/media/whellies01.5b0f83fb.png"
+  }
+]);
